b02f40494f45b0ffbaf36e587c464ad1a8b5b98e
FuniJaerri <62021987+FuniJaerri@users.noreply.github.com>
2021-07-03 21:02:42 +0700

    commit before reworking on everything cuz i stepped on shit

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,15 +1,28 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Item itemType;
+
     public static bool tooltipOn = false;
     GameObject tooltip;
 
     void CreateTooltip()
     {
         tooltip = Instantiate(PlayerInventory.tooltipPrefab, transform.parent.parent.parent.parent);
+
+        for (int i = 0; i < tooltip.transform.childCount; i++)
+        {
+            switch (tooltip.transform.GetChild(i).gameObject.name)
+            {
+                case "CatagoryColorPanel":
+                    tooltip.transform.GetChild(i).GetComponent<Image>().color = Color.green;
+                    break;
+            }
+        }
         tooltip.name = gameObject.name + "'s tooltip panel";
     }
 
@@ -34,10 +47,22 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
     void Update()
     {
+        Vector2 tooltipPos = Input.mousePosition;
+
+        Canvas canvas = gameObject.GetComponentInParent<Canvas>();
+        RectTransform canvasRectTrans = canvas.GetComponent<RectTransform>();
+        RectTransform rect = tooltip.GetComponent<RectTransform>();
+
+        float rectSizeX = canvas.scaleFactor * rect.sizeDelta.x;
+        float rectSizeY = canvas.scaleFactor * rect.sizeDelta.y;
+
+        if (tooltipPos.x + rectSizeX > canvasRectTrans.rect.width) tooltipPos.x = canvasRectTrans.rect.width - rectSizeX;
+        if (tooltipPos.y - rectSizeY < 0) tooltipPos.y = rectSizeY;
+
         if (tooltipOn)
         {    
-            if (Player.cursorObject.activeSelf) Player.cursorObject.SetActive(false);
-            tooltip.GetComponent<RectTransform>().position = Input.mousePosition;
+            if (Player.cursorObject.activeSelf) Player.cursorObject.SetActive(false); 
+            tooltip.GetComponent<RectTransform>().position = tooltipPos;
         }
         else if (!tooltipOn)
         {
