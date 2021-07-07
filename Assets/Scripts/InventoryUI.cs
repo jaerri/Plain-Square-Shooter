ddbe7c3f5534a9ca21947918851d46b324f0c6a6
FuniJaerri <62021987+FuniJaerri@users.noreply.github.com>
2021-07-07 22:26:28 +0700

    Inventory Total Rework
    tooltips data changed too much that i cant log

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Inventory inventory;
+    public GameObject slotPrefab;
+    public Dictionary<Item, GameObject> itemSlots = new Dictionary<Item, GameObject>();
+
+    public void UpdateSlot(Item item)
+    {
+        GameObject slot;
+        List<ItemSlotData> itemList = inventory.inventoryList;
+        if (itemSlots.ContainsKey(item)) slot = itemSlots[item];
+        else slot = Instantiate(slotPrefab, transform);
+
+        slot.name = item.name;
+        slot.GetComponent<InventorySlotUI>().item = item;
+        
+        Image displayImage = slot.GetComponent<Image>();
+        if (item && displayImage)
+        {
+            displayImage.sprite = item.icon;
+            displayImage.color = Color.white;
+            displayImage.GetComponentInChildren<Text>().text = itemList.Find(slot => slot.item = item).amount.ToString();
+        }
+        else
+        {
+            displayImage.sprite = null;
+            displayImage.color = Color.clear;
+            displayImage.GetComponentInChildren<Text>().text = "0";
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) 
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
